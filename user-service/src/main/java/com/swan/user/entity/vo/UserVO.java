0e4468e99630dba6effee6ebf44945b44a044b00
vishwasraiborde19 <103375323+vishwasraiborde19@users.noreply.github.com>
2022-05-23 17:30:39 +0100

    repo tests completed -

---
+++ user-service/src/main/java/com/swan/user/entity/vo/UserVO.java
@@ -0,0 +1,22 @@
+package com.swan.user.entity.vo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserVO {
+	
+	
+	private Long Id;
+	private String username;
+	private String firstName;
+	private String lastName; 
+	
+
+}
---
+++ user-service/src/test/java/com/swan/user/repository/UserRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.swan.user.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.swan.user.entity.domain.User;
+
+@SpringBootTest
+public class UserRepositoryTest {
+
+	// User user ;
+	private String username = "test_user_name";
+	private String fistName = "test_first_name";
+	private String lastName = "test_last_name";
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void testCreateUser() {
+		User user = new User();
+		user.setUsername(username);
+		user.setFirstName(fistName);
+		user.setLastName(lastName);
+
+		user = userRepository.save(user);
+
+		assertTrue(user != null);
+	}
+
+	@Test
+	public void testUserInfo() {
+
+		User user = new User();
+		user.setUsername(username);
+		user.setFirstName(fistName);
+		user.setLastName(lastName);
+
+		user = userRepository.save(user);
+
+		Optional<User> foundUser = userRepository.findById(user.getId());
+		assertTrue("test_user_name".equals(foundUser.get().getUsername()));
+	}
+
+	@Test
+	public void testUpdateUser() {
+
+		User user = new User();
+		user.setUsername(username);
+		user.setFirstName(fistName);
+		user.setLastName(lastName);
+		user = userRepository.save(user);
+
+		User foundUser = userRepository.findById(user.getId()).get();
+		foundUser.setFirstName("updated_user_name");
+		User updatedUser = userRepository.save(foundUser);
+
+		assertTrue("updated_user_name".equals(updatedUser.getFirstName()));
+	}
+
+	@Test
+	public void testListAllUsers() {
+
+		List<User> users = userRepository.findAll();
+
+		assert (users.size() > 0);
+	}
+
+}
