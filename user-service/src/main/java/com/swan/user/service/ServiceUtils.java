846a1772fb4e00817a0472c3dc8c2dd1b1f39dcb
vishwasraiborde19 <103375323+vishwasraiborde19@users.noreply.github.com>
2022-05-23 19:59:22 +0100

    utils and response

---
+++ user-service/src/main/java/com/swan/user/service/ServiceUtils.java
@@ -0,0 +1,26 @@
+package com.swan.user.service;
+
+import org.springframework.beans.BeanUtils;
+
+import com.swan.user.entity.domain.User;
+import com.swan.user.entity.vo.UserVO;
+
+
+/**
+ * class to copy the domain entity to value and value to domain entity
+ * */
+public class ServiceUtils {
+	
+	public static User getEntity(UserVO valueObject) {
+		User user = new User();
+		BeanUtils.copyProperties(valueObject, user);
+		return user;
+	}
+	
+	public static UserVO getValue(User domainObject) {
+		UserVO userVO = new UserVO();
+		BeanUtils.copyProperties(domainObject, userVO);
+		return userVO;
+	}
+
+}
